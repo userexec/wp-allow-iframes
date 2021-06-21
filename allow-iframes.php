9e93e17af37be3f67cc1d6632126d369952315d7
Joshua Woehlke <joshua.woehlke@gmail.com>
2021-06-21 07:54:58 -0700

    initial commit

---
+++ allow-iframes.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Allow iframe Tags in Editor
+Description: Allows use of iframe tags in Custom HTML block for non-super-admins.
+Author: Joshua Woehlke
+*/
+
+function allow_iframes( $allowedposttags ){
+
+	$allowedposttags['iframe'] = array(
+		'align' => true,
+		'allow' => true,
+		'allowfullscreen' => true,
+		'class' => true,
+		'frameborder' => true,
+		'height' => true,
+		'id' => true,
+		'marginheight' => true,
+		'marginwidth' => true,
+		'name' => true,
+		'scrolling' => true,
+		'src' => true,
+		'style' => true,
+		'width' => true,
+		'allowFullScreen' => true,
+		'class' => true,
+		'frameborder' => true,
+		'height' => true,
+		'mozallowfullscreen' => true,
+		'src' => true,
+		'title' => true,
+		'webkitAllowFullScreen' => true,
+		'width' => true
+	);
+
+	return $allowedposttags;
+}
+
+add_filter( 'wp_kses_allowed_html', allow_iframes, 1 );
+
+?>
---
+++ readme.md
@@ -0,0 +1,11 @@
+# WordPress Allow iframe Tags in Editor
+
+If WordPress is stripping iframes out of your users' Custom HTML blocks, this plugin fixes that by setting iframes as allowed tags.
+
+## A note on security
+
+Please note that stripping iframes may actually be a desired behavior for security. This plugin is not in the wp.org listing because it's not entirely clear it jives with the "All code in the directory should be made as secure as possible" guideline. Use this plugin only if you're working with known users who have legitimate reasons to be posting iframes.
+
+## Installation
+
+Clone this project into your `wp-includes/plugins` folder, then activate the plugin within the WordPress plugins menu.
